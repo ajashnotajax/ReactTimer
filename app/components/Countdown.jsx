a29dcdfcc543a5bc85853185ba6bf30f6a756f6f
Ajay sherawat <sherawatajay4@gmail.com>
2017-10-04 16:58:22 +0530

    Add timer and countdown

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+  render: function(props){
+    return(
+      <div>
+        <h1>Countdown</h1>
+      </div>
+
+    );
+  }
+});
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var Timer = React.createClass({
+  render: function(props){
+    return(
+      <div>
+        <h1>Timer</h1>
+      </div>
+
+    );
+  }
+});
+
+module.exports = Timer;
